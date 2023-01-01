5ce0809e117e7922570622a5d7e55d446b5c887c
Gabriel Nascimento <121530351+gnsbriel@users.noreply.github.com>
2023-01-01 20:44:20 -0300

    Generate dynamic table on click

--- public/script/home.js
+++ public/script/home.js
@@ -1,3 +1,41 @@
-document.getElementById("f2").onclick = () => {
-    location.href = "/";
-}
+document.getElementById('expenses').onclick = () => {
+
+    const table = document.getElementById("expensesTable");
+    const tableBody = document.getElementById('expensesTable:body');
+    const tbodyRowCount = table.tBodies[0].rows.length;
+
+    const row = document.createElement('tr');
+
+    for (let j = 0; j < 3; j++) {
+
+        cellsId = [
+            '__desc_',
+            '__value_'
+        ]
+
+        cellsPlaceholder = [
+            'Description',
+            'Amount'
+        ];
+
+        cellsType = [
+            'text',
+            'number'
+        ];
+
+        const cell = document.createElement('td');
+        if ( j != 2) {
+            const input = document.createElement('input');
+            cell.appendChild(input);
+            input.id = (`${cellsId[j]}${tbodyRowCount}`);
+            input.placeholder = (`${cellsPlaceholder[j]}`);
+            input.type = (`${cellsType[j]}`);
+        } else {
+            const trash = document.createElement('button');
+            cell.appendChild(trash).textContent = "🚮";
+        }
+
+        row.appendChild(cell);
+    };
+    tableBody.appendChild(row);
+};
