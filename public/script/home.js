e61b79aab62d6f263d07422d5d8933b96b36eed7
Gabriel Nascimento <121530351+gnsbriel@users.noreply.github.com>
2023-01-07 20:46:29 -0300

    Stoped using array for html atributes

--- public/script/home.js
+++ public/script/home.js
@@ -6,37 +6,36 @@ document.getElementById('add_new_expense').onclick = () => {
     const tbodyRowCount = table.tBodies[0].rows.length;
 
     const row = document.createElement('tr');
-
     for (let j = 0; j < 3; j++) {
-
-        cellsId = [
-            '__desc_',
-            '__value_'
-        ]
-
-        cellsPlaceholder = [
-            'Description',
-            'Amount'
-        ];
-
-        cellsType = [
-            'text',
-            'number'
-        ];
-
         const cell = document.createElement('td');
-        if ( j != 2) {
+        if ( j == 0) {
             const input = document.createElement('input');
             cell.appendChild(input);
+            input.id = (`__desc_${tbodyRowCount}`);
+            input.placeholder = ('Description');
+            input.type = ('text');
+            input.setAttribute('class', '__desc_');
+        } else if (j == 1) {
+            const input = document.createElement('input');
+            cell.appendChild(input);
+            input.id = (`__value_${tbodyRowCount}`);
+            input.placeholder = ('Amount');
+            input.type = ('number');
+            input.setAttribute('class', '__value_');
             input.id = (`${cellsId[j]}${tbodyRowCount}`);
             input.placeholder = (`${cellsPlaceholder[j]}`);
             input.type = (`${cellsType[j]}`);
         } else {
+        } else if (j == 2) {
             const trash = document.createElement('button');
-            cell.appendChild(trash).textContent = "🚮";
-        }
-
+            cell.appendChild(trash);
+            trash.textContent = "❌";
+            trash.id = (`__trash_${tbodyRowCount}`);
+            trash.setAttribute('class', '__trash_');
+        };
         row.appendChild(cell);
+        row.id = (`__row_${tbodyRowCount}`);
+        row.setAttribute('class', '__row_');
     };
     tableBody.appendChild(row);
 };
